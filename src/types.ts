c4a35fee1c7cae6aec1f6bc3995c4980b5a94105
@mikos.zh8 <mikos.zh8@gmail.com>
2022-04-28 14:54:28 +0600

    test git

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,11 @@
 
 export const TYPES = {
-    Application: Symbol.for('Application'),
     ILogger: Symbol.for('ILogger'),
     UserController: Symbol.for('UserController'),
     ExeptionFilter: Symbol.for('ExeptionFilter')
+}
+
+let string;
+export const TYP = {
+    tt: string
 }
